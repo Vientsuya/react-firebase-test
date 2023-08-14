813f06f7e4067d60c2bff7c411b3ff91cade29a3
Marcin Dzierwa <vientvotredouleur@gmail.com>
2023-08-14 12:42:32 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <p className="text-red-500">siema</p>
+    </>
+  );
+}
+
+export default App;
---
+++ src/firebase.config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAobVrHd48aKMuL6sT2c3e31aczJSGskFo",
+  authDomain: "react-firebase-test-1a338.firebaseapp.com",
+  projectId: "react-firebase-test-1a338",
+  storageBucket: "react-firebase-test-1a338.appspot.com",
+  messagingSenderId: "304830879049",
+  appId: "1:304830879049:web:4eeec460bad97b55d63698",
+  measurementId: "G-S9ESMR6GP2",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
